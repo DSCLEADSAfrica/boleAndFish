361f4b6fa968c1a58d64b93c13a518b44108ad75
lewis-munyi <lewismunyi97@gmail.com>
2018-08-22 15:22:06 +0300

    Created Home, Login and Register components

---
+++ resources/assets/js/components/home.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <b>Hello home</b>
+    );
+};
+
+export default Home;
---
+++ resources/assets/js/components/register.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Register = () => {
+    return (
+        <div id="app">
+            <b>This is register component!</b>
+        </div>
+    );
+};
+
+export default Register;
